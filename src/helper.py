2503f623f10db36c396c3aeacd6ee42593785e0f
Deshai Pranav <pranavnrpt@gmail.com>
2022-02-08 12:51:46 +0530

    Add optimizations

--- src/helper.py
+++ src/helper.py
@@ -1,12 +1,22 @@
 from random import shuffle
 
 
-def get_next_positions(board):
-    """Return a list of allowed positions for the given board state."""
+similar_positions = {
+    (0, 1, 2, 3, 4, 5, 6, 7, 8),
+    (2, 5, 8, 1, 4, 7, 0, 3, 6),  # Rotate 90 degrees anti-clockwise
+    (8, 7, 6, 5, 4, 3, 2, 1, 0),  # Rotate 180 degrees anti-clockwise
+    (6, 3, 0, 7, 4, 1, 8, 5, 2),  # Rotate 270 degrees anti-clockwise
+    (2, 1, 0, 5, 4, 3, 8, 7, 6),  # Flip horizontally
+    (6, 7, 8, 3, 4, 5, 0, 1, 2),  # Flip vertically
+    (0, 3, 6, 1, 4, 7, 2, 5, 8),  # Transpose-1
+    (8, 5, 2, 7, 4, 1, 6, 3, 0)  # Transpose-2
+}
 
-    next_positions = [i for i, p in enumerate(board) if p == 0]
-    shuffle(next_positions)
-    return next_positions
+
+def get_next_moves(board):
+    """Return a list of allowed moves for the given board state."""
+
+    return [i for i, p in enumerate(board) if p == 0]
 
 
 def get_winner(board):
@@ -16,7 +26,7 @@ def get_winner(board):
     """
 
     # Horizontal check
-    for i in [0, 3, 6]:
+    for i in (0, 3, 6):
         if board[i] == board[i+1] and board[i+1] == board[i+2]:
             if board[i] == 1:
                 return 1
@@ -37,25 +47,64 @@ def get_winner(board):
             return 1
         elif board[0] == 2:
             return 2
-
     if board[2] == board[4] and board[4] == board[6]:
         if board[2] == 1:
             return 1
         elif board[2] == 2:
             return 2
 
-    if not get_next_positions(board):
+    if not get_next_moves(board):
         return 0
 
     return None
 
 
+def get_best_move(board, next_moves, maximizer, minimizer, cache, depth):
+    """Return best move for the given board state.
+
+    `depth`: Depth limit for minimax search.
+    """
+
+    # Similar positions
+    for x in similar_positions:
+        tmp = tuple(board[i] for i in x)
+        if tmp in cache:
+            return x[cache[tmp]]
+
+    best_value = -10000
+    best_move = None
+
+    for move in next_moves:
+        board[move] = maximizer
+
+        # Forced moves
+        if get_winner(board) == maximizer:
+            board[move] = 0
+            return move
+
+        cur_value = minimax(
+            board[:], False, -1000, 1000, maximizer, minimizer, 0, depth)
+
+        board[move] = 0
+
+        if cur_value > best_value:
+            best_value = cur_value
+            best_move = move
+
+    cache[tuple(board)] = best_move
+
+    return best_move
+
+
 def minimax(board, max_turn, alpha, beta, maximizer, minimizer, cur_depth, req_depth):
     evaluate = {None: 0, 0: 0, maximizer: 100, minimizer: -100}
     score = evaluate[get_winner(board)]
-    next_positions = get_next_positions(board)
+    next_moves = get_next_moves(board)
 
-    # Optimization 3: Quicker wins
+    # Randomize search order
+    shuffle(next_moves)
+
+    # Quicker wins
     if score == 100:
         return score - cur_depth
     elif score == -100:
@@ -66,13 +115,13 @@ def minimax(board, max_turn, alpha, beta, maximizer, minimizer, cur_depth, req_d
 
     if max_turn:
         best = -10000
-        for position in next_positions:
-            board[position] = maximizer
+        for move in next_moves:
+            board[move] = maximizer
             best = max(best, minimax(board, not max_turn,
                        alpha, beta, maximizer, minimizer, cur_depth+1, req_depth))
-            board[position] = 0
+            board[move] = 0
 
-            # Optimization 2: Alpha-beta pruning
+            # Alpha-beta pruning
             alpha = max(alpha, best)
             if alpha >= beta or cur_depth == req_depth:
                 break
@@ -80,51 +129,14 @@ def minimax(board, max_turn, alpha, beta, maximizer, minimizer, cur_depth, req_d
         return best
     else:
         best = 10000
-        for position in next_positions:
-            board[position] = minimizer
-
+        for move in next_moves:
+            board[move] = minimizer
             best = min(best, minimax(board, not max_turn,
                        alpha, beta, maximizer, minimizer, cur_depth+1, req_depth))
-
-            board[position] = 0
+            board[move] = 0
 
             beta = min(beta, best)
             if alpha >= beta or cur_depth == req_depth:
                 break
 
         return best
-
-
-def get_best_position(board, maximizer, minimizer, cache, depth):
-    """Return best position for the given board state.
-
-    `depth`: Depth limit for minimax search.
-    """
-
-    # Optimization 1: Caching
-    if str(board) in cache:
-        return cache[str(board)]
-
-    best_value = -10000
-    best_position = None
-
-    for position in get_next_positions(board):
-        board[position] = maximizer
-
-        # Optimization 4: Instant wins
-        if get_winner(board) == maximizer:
-            board[position] = 0
-            return position
-
-        position_value = minimax(
-            board[:], False, -1000, 1000, maximizer, minimizer, 0, depth)
-
-        board[position] = 0
-
-        if position_value > best_value:
-            best_value = position_value
-            best_position = position
-
-    cache[str(board)] = best_position
-
-    return best_position
