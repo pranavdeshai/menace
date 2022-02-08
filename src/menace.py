dd48df361c4ed6c9f0225a06c715cac99b0328f3
Deshai Pranav <pranavnrpt@gmail.com>
2022-02-08 12:54:42 +0530

    Remove plotting and add CLI support

--- src/menace.py
+++ src/menace.py
@@ -1,82 +1,86 @@
-from helper import get_next_positions, get_winner, get_best_position
+from helper import get_next_moves, get_best_move, get_winner
 from random import choices
-import matplotlib.pyplot as plt
+from sys import argv
 
 
-def simulate(n=1, d=0):
-    """`n`: Number of games to simulate. Default is 1.
+def simulate(m=0, n=100):
+    """Simulate games and return the results.
 
-    `d`: Depth limit for minimax search. Default is 0 (random).
-        -1 for no limit,
+    `m`: Mode of gameplay. Default is 0 (random).
+        [-1, 0, ...] sets depth limit for minimax search. -1 for no limit.
         -2 to simulate against MENACE and 
         -3 to play against human.
+    `n`: Number of games to simulate. Default is 100.
     """
 
     scoreboard = [0, 0, 0]  # Draws, wins and losses (wrt MENACE)
     changes = []
     cache = {}
     menace = {}
-    if d == -2:
+
+    if m == -2:
         menace2 = {}
 
+    players = [1, 2]
     for i in range(n):
-        board = [0 for i in range(9)]
-        menace_moves = []
-        if d == -2:
-            menace2_moves = []
+        board = [0 for _ in range(9)]
+        menace_moves = set()
+        if m == -2:
+            menace2_moves = set()
 
         game_over = False
         while not game_over:
-            for player in [1, 2]:
+            for player in players:
                 winner = get_winner(board)
                 if winner is None:
-                    next_positions = get_next_positions(board)
+                    next_moves = get_next_moves(board)
 
                     if player == 1:
                         # Add new board states to MENACE
-                        state = ''.join(str(i) for i in board)
+                        state = tuple(board)
                         if state not in menace:
                             menace[state] = {}
-                            for pos in next_positions:
-                                menace[state][pos] = 1
+                            for move in next_moves:
+                                menace[state][move] = 1
 
                         # Get a move from MENACE
-                        p = [i for i in menace[state].keys()]
-                        w = [i for i in menace[state].values()]
-                        position = choices(p, w)[0]
+                        p = [_ for _ in menace[state].keys()]
+                        w = [_ for _ in menace[state].values()]
+                        move = choices(p, w)[0]
 
                         # Store the played moves
-                        menace_moves.append((state, position))
+                        menace_moves.add((state, move))
                     else:
-                        if d == -2:
+                        if m == -2:
                             # Add new board states to MENACE-2
-                            state = ''.join(str(i) for i in board)
+                            state = tuple(board)
                             if state not in menace2:
                                 menace2[state] = {}
-                                for next_position in next_positions:
-                                    menace2[state][next_position] = 1
+                                for move in next_moves:
+                                    menace2[state][move] = 1
 
-                            p = [i for i in menace2[state].keys()]
-                            w = [i for i in menace2[state].values()]
-                            position = choices(p, w)[0]
+                            p = [_ for _ in menace2[state].keys()]
+                            w = [_ for _ in menace2[state].values()]
+                            move = choices(p, w)[0]
 
-                            menace2_moves.append((state, position))
-                        elif d == -3:
+                            menace2_moves.add((state, move))
+                        elif m == -3:
                             print(f'{board[:3]}\n{board[3:6]}\n{board[6:]}\n')
 
                             # Get input from user
-                            position = int(input('Move: '))
+                            move = int(input('Move: '))
 
                             # Validate input
-                            while position not in next_positions:
-                                position = int(
-                                    input('Invalid move. Try again: '))
+                            while move not in next_moves:
+                                move = int(
+                                    input(f'Invalid move. Choose from the below\n{next_moves}: '))
 
                         else:
                             # Play the best move
-                            position = get_best_position(board, 2, 1, cache, d)
+                            move = get_best_move(
+                                board, next_moves, 2, 1, cache, m)
 
-                    board[position] = player
+                    board[move] = player
                 else:
                     scoreboard[winner] += 1
                     game_over = True
@@ -87,14 +91,14 @@ def simulate(n=1, d=0):
                     # Reinforce MENACE
                     for s, i in menace_moves:
                         if winner == 1:
-                            menace[s][i] += 3  # Reward if won
+                            menace[s][i] += 3
                         elif winner == 2:
                             if menace[s][i] > 1:
-                                menace[s][i] -= 1  # Punish if lost
+                                menace[s][i] -= 1
                         else:
                             menace[s][i] += 1
 
-                    if d == -2:
+                    if m == -2:
                         for s, i in menace2_moves:
                             if winner == 2:
                                 menace2[s][i] += 3
@@ -108,19 +112,11 @@ def simulate(n=1, d=0):
     return changes
 
 
-opponents = {
-    0: 'Random',
-    1: 'Depth 1',
-    5: 'Depth 5',
-    -1: 'Perfect',
-    -2: 'MENACE',
-}
+params = []
+for arg in argv[1:]:
+    try:
+        params.append(int(arg))
+    except:
+        print(f'Ignoring invalid argument: {arg}')
 
-for x in opponents:
-    changes = simulate(100, x)
-    plt.plot(changes)
-
-plt.xlabel('Number of games')
-plt.ylabel('Change in number of beads in first box')
-plt.legend([opponents[x] for x in opponents], loc='upper left')
-plt.show()
+simulate(*params)
